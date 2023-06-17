800d994de67285294a62bf7f516374ead63a7a64
Mark <>
2023-06-17 18:15:18 +0200

    feat: implementation hash fn

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from 'crypto';
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'path';
+
+const FILE_PATH = resolve(`./src/hash/files/fileToCalculateHashFor.txt`);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileContent = await readFile(FILE_PATH, 'utf-8');
+    const hash = createHash('SHA256').update(fileContent);
+
+    console.log(`${hash.digest('hex')}`);
 };
 
 await calculateHash();
