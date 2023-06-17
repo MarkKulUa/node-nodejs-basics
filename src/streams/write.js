6bb3f7fd23acb92abd59c56fe9ed16c603020d26
Mark <>
2023-06-17 21:16:47 +0200

    feat: implement streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { resolve } from 'path';
+
+const FILE_PATH = resolve(`./src/streams/files/fileToWrite.txt`);
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(FILE_PATH);
+
+    await pipeline(process.stdin, stream);
 };
 
 await write();
