3f72c0374dfeb6b40a6658f749cb44d26a263702
Mark <>
2023-06-17 19:01:41 +0200

    feat: implementation cli scripts

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process
+        .argv
+        .slice(2);
+
+    const result = args
+        .reduce((acc, item, key, arr) => {
+
+            if (item.startsWith('--')) {
+                acc.push(`${item.replace('--', '')} is ${arr[key+1]}`);
+            }
+
+            return acc;
+        }, [])
+        .join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = Object.entries(process.env);
+
+    const result = env
+        .reduce((acc, [item, value]) => {
+
+            if (item.startsWith('RSS_')) {
+                acc.push(`${item}=${value}`);
+            }
+
+            return acc;
+        }, [])
+        .join('; ');
+
+    console.log(result);
 };
 
 parseEnv();
