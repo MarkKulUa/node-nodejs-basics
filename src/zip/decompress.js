a6c48f71d79e73c144d30bab23995adcd931a020
Mark <>
2023-06-17 21:28:14 +0200

    feat: implement zip/unzip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGunzip } from 'zlib';
+import { resolve } from 'path';
+
+const FILE_PATH = resolve(`./src/zip/files/fileToCompress.txt`);
+const FILE_ARCH_PATH = resolve(`./src/zip/files/fileCompressed.gz`);
+
+
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(createReadStream(FILE_ARCH_PATH), createGunzip(), createWriteStream(FILE_PATH));
 };
 
 await decompress();
