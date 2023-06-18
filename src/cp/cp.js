3a18891f854dae45fcaf1e599cb1712c4a1da6f7
Mark <>
2023-06-18 09:21:01 +0200

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from 'child_process';
+import { resolve } from "path";
+
+const FILE_PATH = resolve(`./src/cp/files/script.js`);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const forkObjChildProcess = fork(FILE_PATH, args, { silent: true });
+
+    process.stdin.pipe(forkObjChildProcess.stdin);
+
+    forkObjChildProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['test_arg1', 'test_arg2']);
