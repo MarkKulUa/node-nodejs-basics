58e40c37cb6640b1f77c357549372f1477254938
Mark <>
2023-06-18 08:44:05 +0200

    feat: implement wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { resolve } from "path";
+
+const FILE_PATH = resolve(`./src/wt/worker.js`);
+const CALCULATE_START_NUMBER = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const calculateFibonacciResult = (workerData) => new Promise((resolve) => {
+        const worker = new Worker(FILE_PATH, {workerData});
+
+        worker.on('message', (result) => resolve({ status: 'calculated', result: result}));
+        worker.on('error', () => resolve({ status: 'error', result: false}));
+    });
+
+    const calcPromises = cpus()
+        .map((value, key) => calculateFibonacciResult(CALCULATE_START_NUMBER + key));
+
+    const result = await Promise.all(calcPromises);
+
+    console.log(result);
 };
 
 await performCalculations();
